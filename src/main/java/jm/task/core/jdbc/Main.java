7079e9594a8737ba33e32ac29dace0df64fc1260
ibraimov2000 <mibraimov250@gmail.com>
2021-09-23 23:04:58 +0600

    Realized HibernateImpl class. Добавлен HQL запрос....

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,8 +2,10 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserServiceImpl;
 
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {//
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {//
         UserServiceImpl userDaoHibernate = new UserServiceImpl();
         userDaoHibernate.createUsersTable();
         userDaoHibernate.saveUser("Ivan", "Ivanov", (byte) 5);
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,32 +1,34 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+    UserDao userDaoHibernate = new UserDaoHibernateImpl();
 
-    public void createUsersTable() {
+    public void createUsersTable() throws SQLException, ClassNotFoundException {
         userDaoHibernate.createUsersTable();
     }
 
-    public void dropUsersTable() {
+    public void dropUsersTable() throws SQLException, ClassNotFoundException {
         userDaoHibernate.dropUsersTable();
     }
 
-    public void saveUser(String name, String lastName, byte age) {
+    public void saveUser(String name, String lastName, byte age) throws SQLException, ClassNotFoundException {
         userDaoHibernate.saveUser(name, lastName, age);
     }
 
-    public void removeUserById(long id) {
+    public void removeUserById(long id) throws SQLException, ClassNotFoundException {
         userDaoHibernate.removeUserById(id);
     }
 
-    public List<User> getAllUsers() { return userDaoHibernate.getAllUsers(); }
+    public List<User> getAllUsers() throws SQLException, ClassNotFoundException { return userDaoHibernate.getAllUsers(); }
 
     public void cleanUsersTable() {
         userDaoHibernate.cleanUsersTable();
